5493bb7a4ec434186795f42cc0d32b341f764434
Timothy Caballa <tfcaballa@gmail.com>
2022-02-18 15:55:25 +0800

    update footer

--- footer.php
+++ footer.php
@@ -2,7 +2,13 @@
 				</div>
 			</div>
 		</section>
-
+		<footer id="footer">
+			<div class="container">
+				<div class="row">
+					<p class="text-center"> 2022 © PLAUNT </p>
+				</div>
+     		 </div>
+		</footer>
 		<!-- jQuery -->
 		<script src="assets/js/jquery.js"></script>
 		<!-- Bootstrap JavaScript -->
